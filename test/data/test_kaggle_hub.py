e57190b05e85a6f669c6ff827616309c3765a746
SteveVu <phucvhd@gmail.com>
2025-09-26 22:07:31 -0700

    Init kaggle hub

---
+++ test/data/test_kaggle_hub.py
@@ -0,0 +1,13 @@
+import pytest
+
+from data.kaggle_hub import KaggleHub
+
+kaggle_hub = KaggleHub()
+
+def test_load_dataset():
+    try:
+        actual = kaggle_hub.load_dateset("mlg-ulb/creditcardfraud", "creditcard.csv")
+        assert actual is not None
+        assert not actual.empty  # Optional: ensure DataFrame isn't empty
+    except Exception as e:
+        pytest.fail(f"Unexpected error: {e}")
