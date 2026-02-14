f11b2c363ec1646d50050cc90502c3714a6bc4a8
SteveVu <75359238+phucvhd@users.noreply.github.com>
2026-02-14 15:44:18 -0800

    add hyper tuning v3 (#15)

---
+++ runs/fraud_hyper_tuning_v3.py
@@ -0,0 +1,10 @@
+from config.config_loader import ConfigLoader
+from pipelines.fraud_pipeline import FraudPipeline
+from runs.setup import pipeline_env_setup
+
+if __name__ == '__main__':
+    config_loader = ConfigLoader()
+    pipeline_env_setup()
+
+    fraud_pipeline = FraudPipeline(config_loader=config_loader)
+    fraud_pipeline.run_mlflow_hyper_tune_v3()
